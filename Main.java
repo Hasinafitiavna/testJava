97bba05e3f9e155ff751359d138a96409bbcae96
hasina <hasinafitiavana2002@gmail.com>
2023-08-12 14:45:46 +0200

    commit

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main{
     public static int addition(int a,int b){
         return a+b;
     }
+    public static void bonjour(){
+        System.out.println("bonjour");
+    }
     public static void main(String[] args){
         System.out.println("test");
     }
